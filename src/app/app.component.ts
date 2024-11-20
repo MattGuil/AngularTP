457f97c7ac75f0184b099072b3e260106260a9f0
MattGuil <guillemin.matt@gmail.com>
2024-11-20 12:03:04 +0100

    tp2 almost completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EtudiantComponent } from './components/etudiant/etudiant.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EtudiantComponent, CommonModule],
+  imports: [RouterOutlet, EtudiantComponent, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'tp1';
   estAuth = false;
+  etat = "absent";
+  commentaireSeance = "";
 
   students = [
     { nomEtu: 'Ghomashi', prenomEtu: 'Houman', statutEtu: 'absent' },
@@ -21,17 +24,22 @@ export class AppComponent {
   ];
 
   constructor() {
-    setTimeout(() => {
-      this.estAuth = true;
-    }, 5000);
-
     console.log(this.students);
   }
 
   rendTousPresents() {
     this.students.forEach(student => {
-      student.statutEtu = 'présent';
+      student.statutEtu = 'present';
     });
+    this.etat = "present";
     console.log("Tous les étudiants sont présents !");
+  }
+
+  rendTousAbsents() {
+    this.students.forEach(student => {
+      student.statutEtu = 'absent';
+    });
+    this.etat = "absent";
+    console.log("Tous les étudiants sont absents !");
   }
 }
--- src/app/components/etudiant/etudiant.component.ts
+++ src/app/components/etudiant/etudiant.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-etudiant',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './etudiant.component.html',
   styleUrl: './etudiant.component.scss'
 })
@@ -12,6 +13,7 @@ export class EtudiantComponent {
   @Input() nomEtu = "Bon";
   @Input() prenomEtu = "Jean";
   @Input() statutEtu = "absent";
+  @Input() commentaire = "";
 
   retNomEtu(): string {
     return this.nomEtu;
@@ -23,5 +25,9 @@ export class EtudiantComponent {
 
   retStatutEtu(): string {
     return this.statutEtu;
+  }
+
+  retCommentaire(): string {
+    return this.commentaire;
   }
 }
